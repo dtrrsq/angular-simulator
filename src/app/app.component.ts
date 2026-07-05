79eb4e23f24cbf0b7b9e537a5b7b4babf1cdd5e9
Muhammad <magomedgadisov95@gmail.com>
2026-07-05 21:10:51 +0300

    added date, clicker, live-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Colors } from '../enums/color';
 import { tourCollection } from '../collection';
 import { memberCollection } from '../collection';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  tourLocation: string = '';
+  tourDate: string = '';
+  tourMembers: string = '';
   companyName: string = 'РУМТИБЕТ';
   tours = tourCollection;
   members = memberCollection;
+  clickCount: number = 0;
+  isCounterVisible: boolean = false;
+  liveInput: string = '';
+  isLoading: boolean = true;
+
+  tourProgram = [
+    {
+      id: 1,
+      label: 'наше предложение',
+      title: 'Лучшие программы для тебя',
+      description:
+        'Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа.',
+      buttonText: 'Стоимость программ',
+
+      features: [
+        {
+          id: 1,
+          img: '/images/guide-icon.png',
+          title: 'Опытный гид',
+          desc: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+        },
+        {
+          id: 2,
+          img: '/images/shield-icon.png',
+          title: 'Безопасный поход',
+          desc: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+        },
+        {
+          id: 3,
+          img: '/images/price-icon.png',
+          title: 'Лояльные цены',
+          desc: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+        },
+      ],
+    },
+  ];
+
+  constructor() {
+    this.saveLastVisitDate();
+    this.saveVisitCount();
+  }
 
   public isCurrentColor(color: Colors): boolean {
     if (color === Colors.GREEN || color === Colors.BLUE || color === Colors.RED) {
@@ -35,8 +81,33 @@ export class AppComponent {
     localStorage.setItem('visitCount', newCount.toString());
   }
 
-  constructor() {
-    this.saveLastVisitDate();
-    this.saveVisitCount();
+  currentDateTime: Date = new Date();
+  private timerId: any;
+
+  ngOnInit() {
+    this.timerId = setInterval(() => {
+      this.currentDateTime = new Date();
+    }, 1000);
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  }
+
+  ngOnDestroy() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  increaseClicks(): void {
+    this.clickCount++;
+    this.isCounterVisible = true;
+  }
+
+  decreaseClicks(): void {
+    if (this.clickCount > 0) {
+      this.clickCount--;
+    }
   }
 }
